b17b1b9552734f995b8db63e165406f89b96b07f
Sandesha H G <Sandesha H G>
2020-02-25 05:04:36 +0530

    deleted duplicate file

--- salesken/src/main/java/com/SaleskenAssignment/salesken/DAO/ProcessRepo.java
+++
@@ -1,7 +0,0 @@
-package com.SaleskenAssignment.salesken.DAO;
-
-import org.springframework.data.repository.CrudRepository;
-import com.SaleskenAssignment.salesken.Model.Process;
-
-public interface ProcessRepo extends CrudRepository<Process,Long> {
-}
--- salesken/src/main/java/com/SaleskenAssignment/salesken/DAO/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.SaleskenAssignment.salesken.DAO;
-
-import com.SaleskenAssignment.salesken.Model.Product;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface ProductRepo extends CrudRepository<Product, Long> {
-}
