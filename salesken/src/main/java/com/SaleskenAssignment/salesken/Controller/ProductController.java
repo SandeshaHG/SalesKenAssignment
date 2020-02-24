ff304d07010164aefac59e7c7b72c3e73a70ec75
Sandesha H G <Sandesha H G>
2020-02-24 20:50:58 +0530

    Added the many to many relation with processes

--- salesken/src/main/java/com/SaleskenAssignment/salesken/Controller/ProductController.java
+++ salesken/src/main/java/com/SaleskenAssignment/salesken/Controller/ProductController.java
@@ -1,18 +1,25 @@
 package com.SaleskenAssignment.salesken.Controller;
 
+import com.SaleskenAssignment.salesken.DAO.ProcessRepo;
 import com.SaleskenAssignment.salesken.DAO.ProductRepo;
+import com.SaleskenAssignment.salesken.Model.Process;
 import com.SaleskenAssignment.salesken.Model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.json.JSONObject;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+@RestController
 public class ProductController {
 @Autowired
 private ProductRepo dao;
+
+@Autowired
+private ProcessRepo processdao;
 
 @GetMapping("/getAllProducts")
 List<Product> getAllProducts(){
@@ -20,15 +27,26 @@ List<Product> getAllProducts(){
 }
 
 @PostMapping("/AddProduct")
-public String AddProduct(@RequestBody String json) {
+public Product AddProduct(@RequestBody String json) {
     JSONObject jsonObj = new JSONObject(json);
     String jsonProductName = jsonObj.getString("product_name");
     String jsonProductNumberOfDocuments = jsonObj.getString("product_number_of_documents");
     String jsonproductDescription = jsonObj.getString("product_description");
     String jsonproductImageName = jsonObj.getString("product_image_name");
-    Product product = new Product(jsonProductName, jsonProductNumberOfDocuments, jsonproductDescription, jsonproductImageName);
-    dao.save(product);
-    return "Product Added";
+    List<Process> processArrayList=new ArrayList<Process>();
+    Product product = new Product(jsonProductName, jsonProductNumberOfDocuments, jsonproductDescription, jsonproductImageName,processArrayList);
+    return dao.save(product);
+}
+
+@PatchMapping("/AddProcess/{productId}/{processId}")
+    public String  AddProcess(@PathVariable long productId,@PathVariable long processId){
+    Process process=processdao.findById(processId).get();
+    Product product=dao.findById(productId).get();
+    List<Process> processArrayList=product.getProcesses();
+    processArrayList.add(process);
+    product.setProcesses(processArrayList);
+    return "Process  '"+process.getProcessName()+"' has been successfully added to Product '" +product.getProductName()+"'";
+
 }
 
 }
--- salesken/src/main/java/com/SaleskenAssignment/salesken/Model/Product.java
+++ salesken/src/main/java/com/SaleskenAssignment/salesken/Model/Product.java
@@ -3,36 +3,57 @@ package com.SaleskenAssignment.salesken.Model;
 import javax.persistence.*;
 
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
-@Table(name = "Products")
+@Table(name = "SaleskenProducts")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="id")
     private long id;
 
+    @NonNull
     @Column(name="product_name")
     private String productName;
 
+    @NonNull
     @Column(name="product_number_of_documents")
     private String productNumberOfDocuments;
 
+    @NonNull
     @Column(name="product_description")
     private String productDescription;
 
+    @NonNull
     @Column(name="product_image_name")
     private String productImageName;
 
-    public Product(String productName, String productNumberOfDocuments, String productDescription, String productImageName) {
+    @ManyToMany(fetch = FetchType.LAZY)
+    private List<Process> processes=new ArrayList<Process>();
+
+    public Product(String productName, String productNumberOfDocuments, String productDescription, String productImageName, List<Process> processes) {
         this.productName = productName;
         this.productNumberOfDocuments = productNumberOfDocuments;
         this.productDescription = productDescription;
         this.productImageName = productImageName;
+        this.processes = processes;
     }
+
+    public List<Process> getProcesses() {
+        return processes;
+    }
+
+    public void setProcesses(List<Process> processes) {
+        this.processes = processes;
+    }
+
 
     public String getProductName() {
         return productName;
@@ -64,6 +85,9 @@ public class Product {
 
     public void setProductImageName(String productImageName) {
         this.productImageName = productImageName;
+    }
+
+    public Product() {
     }
 }
 
